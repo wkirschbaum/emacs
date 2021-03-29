1d4ef56bb6f3d8d25853e0f3f4e288baf4635ab5
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2021-03-29 21:53:02 +0200

    Add whitespace cleanup in progmode

--- modules/native-config.el
+++ modules/native-config.el
@@ -16,6 +16,18 @@
 (setq-default display-line-numbers-widen t)
 (global-display-line-numbers-mode)
 
+(use-package whitespace
+  :hook (prog-mode . whitespace-mode)
+  :config
+  (setq whitespace-style '(face tab-mark trailing empty)))
+
+(defun whk/whitespace-cleanup ()
+  "Cleans white spaces in progmode."
+  (when (derived-mode-p 'prog-mode)
+    (whitespace-cleanup)))
+
+(add-hook 'before-save-hook 'whk/whitespace-cleanup)
+
 ;; Time
 (defvar display-time-string-forms)
 (setq display-time-string-forms
