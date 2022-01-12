c3d444e4b8a0414f7b2a93f05bd15fd7c7afda5d
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2022-01-12 14:21:59 +0200

    Best effort stab handling

--- modules/elixir_test.ex
+++ modules/elixir_test.ex
@@ -1,27 +1,85 @@
+try do
+  foo = bar
+catch
+  _ ->
+    foo
+
+    fn
+      foo ->
+        fo
+
+      _ ->
+        boo
+    end
+
+  false ->
+    another
+end
+
 @foo "bar"
 
+defmodule Foo do
+  def foo do
+    bar
+  end
+
+  defmodule Boo do
+    def ppp() do
+
+    end
+
+    defmodule Zoo do
+      def boo do
+        x = if true do
+              foo
+            else
+              boo
+            end
+      else
+        x = if true do
+              foo
+            else
+              boo
+            end
+      end
+    end
+  end
+end
+
+def foo, do: boo, else: boo
+
 def all?(enumerable) do
-      Enumerable.reduce(enumerable, {:cont, true}, fn entry, _ ->
-                                                     if entry, do: {:cont, true}, else: {:halt, false}
-                                                   end)
-      |> elem(1)
+  Enumerable.reduce(enumerable, {:cont, true}, fn entry, _ ->
+                                                 if entry, do: {:cont, true}, else: {:halt, false}
+                                               end)
+  |> elem(1)
 end
 
 defp zip_with_list([head1 | next1], [head2 | next2], fun) do
-       [
-         fun.(head1, head2) | zip_with_list(next1, next2, fun)
-       ]
+  [
+    fun.(head1, head2) | zip_with_list(next1, next2, fun)
+  ]
 end
 
 @moduledoc """
 Somecomment here
 """
 defmodule Foo.Bar do
-            def foo() do
-                  x = %{
-                      foo: :bar
-                    }
-            end
+  # def reduce(function, acc, fun) when is_function(function, 2), do: function.(acc, fun)
+  def foo, do: boo, else: boo
+
+  foo =
+    1 + 3
+
+  bar = fn foo ->
+          bar
+        end
+
+  def foo() do
+    x = %{
+        foo: :bar
+      }
+  end
 end
 
 @doc """
@@ -32,74 +90,74 @@ end
 """
 # foo bar
 defp status(kyc_status) do
-       case kyc_status do
-       a ->
-         b
-         asdf
-         "foo #{foo = bar}"
+  case kyc_status do
+    a ->
+      b
+      asdf
+      "foo #{foo = bar}"
 
-         x = [
-           "one",
-           "two"
-         ]
+      x = [
+        "one",
+        "two"
+      ]
 
-         sdf
+      sdf
 
-         fn foo ->
-           moo
-         end
+      fn foo ->
+        moo
+      end
 
-       c ->
-         d
+    c ->
+      d
 
-       k ->
-         d
-       end
+    k ->
+      d
+  end
 
-       # foo bar
-       case kyc_status do
-       %KycStatus{status: status} ->
-         status
+  # foo bar
+  case kyc_status do
+    %KycStatus{status: status} ->
+      status
 
-       %KycStatus{status: status} ->
-         status
+    %KycStatus{status: status} ->
+      status
 
-       _ ->
-         :foo
+    _ ->
+      :foo
 
-       _ ->
-         :not_started
+    _ ->
+      :not_started
 
-       _ ->
-         :not_started
+    _ ->
+      :not_started
 
-       _ ->
-         :not_started
-       end
+    _ ->
+      :not_started
+  end
 
-       case kyc_status do
-              %KycStatus{status: :failed} -> :not_started
-              %KycStatus{status: status} -> status
-              _ -> :foo
-              _ -> :not_started
-              _ -> :not_started
-              _ -> :not_started
-       end
+  case kyc_status do
+    %KycStatus{status: :failed} -> :not_started
+    %KycStatus{status: status} -> status
+    _ -> :foo
+    _ -> :not_started
+    _ -> :not_started
+    _ -> :not_started
+  end
 
-       case kyc_status do
-       %KycStatus{status: status} ->
-         status
+  case kyc_status do
+    %KycStatus{status: status} ->
+      status
 
-       _ ->
-         :foo
+    _ ->
+      :foo
 
-       _ ->
-         :not_started
+    _ ->
+      :not_started
 
-       _ ->
-         :not_started
+    _ ->
+      :not_started
 
-       _ ->
-         :not_started
-       end
+    _ ->
+      :not_started
+  end
 end
