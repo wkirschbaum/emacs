47bad1b37fb6e60e5fbce4952f37b5d827083843
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2022-02-06 08:57:34 +0200

    Format elixir test

--- modules/elixir_test.ex
+++ modules/elixir_test.ex
@@ -1,35 +1,31 @@
 def all?(enumerable) do
   Enumerable.reduce(enumerable, {:cont, true}, fn entry, _ ->
-                                                 if entry, do: {:cont, true}, else: {:halt, false}
-                                                 end)
+    if entry, do: {:cont, true}, else: {:halt, false}
+  end)
   |> elem(1)
 end
 
 foo
-
-{a, b, c, d, e, g, h, p, k, t, t, t, t, t, t, t, t, t, t, t, t, t, t, t, t, t} =
-  9
 
 def foo do
   fsdafd
 end
 
 @type foo ::
-  String.t()
+        String.t()
 
+@type result ::
+        {:done, term}
+        | {:halted, term}
+        | {:suspended, term, continuation}
 
-    @type result ::
-    {:done, term}
-      | {:halted, term}
-      | {:suspended, term, continuation}
+4 = 5
 
-      4 = 5
+x =
+  5 +
+    5 -
+    6 * 4
 
-      x =
-        5 +
-          5 -
-          6 * 4
-
-      5
-      |> time
-      |> IO.puts()
+5
+|> time
+|> IO.puts()
