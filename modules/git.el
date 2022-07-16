70a11049291d50f6457e5bb01311ad04794661d6
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2022-07-16 13:48:51 +0200

    Remove tab

--- modules/git.el
+++ modules/git.el
@@ -11,14 +11,13 @@
   :ensure t
   :demand t
   :hook ((dired-mode . diff-hl-dired-mode)
-	 (magit-post-refresh . diff-hl-magit-post-refresh)
+         (magit-post-refresh . diff-hl-magit-post-refresh)
          (magit-post-refresh . diff-hl-magit-pre-refresh))
   :config
   (global-diff-hl-mode))
 
 (use-package git-modes
   :ensure t
-  :config 
+  :config
   (add-to-list 'auto-mode-alist
                (cons "/.dockerignore\\'" 'gitignore-mode)))
-
